4ad6075b618d0c041c054e0eef039a83c9b5ee40
kevin1131 <kevin1131@npixel.co.kr>
2025-01-24 16:36:26 +0900

    로그인 Dto 추가

---
+++ Shared/Shared/Dtos/Login.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Dtos
+{
+    public class LoginRequest
+    {
+        public ulong PlayerId { get; set; }
+    }
+    
+    public class LoginResponse
+    {
+        public string Message { get; set; } = string.Empty;
+    }
+}
