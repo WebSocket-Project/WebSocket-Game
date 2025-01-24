0eee4a55d95a65e4ebb25dc5483e89349fe2e09e
kevin1131 <kevin1131@npixel.co.kr>
2025-01-24 12:24:29 +0900

    ProtocolAttribute 추가

---
+++ Shared/Shared/ProtocolAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Shared
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ProtocolAttribute : Attribute
+    {
+        public Protocol Protocol { get; }
+    
+        public ProtocolAttribute(Protocol protocol)
+        {
+            Protocol = protocol;
+        }
+    }
+}
